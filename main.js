25571a341b28237aa6fa82fe7875af3e17578acb
Pawel-Markowicz <pawel.markowicz88@gmail.com>
2023-01-06 08:56:24 +0100

    chessboard

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
 
+// szachownica
+// let space = "";
+// let sign = "* ";
+// let size = 10;
 
-let space = "";
-let sign = "* ";
-let size = 10;
-
-for(let i = 1; i <= size; i++){
-    if(i % 2 == 0){
-        space += " ";
-    }
-    space += sign;
-    for(let i = size; i <= size; i++ ){
-        space += sign.repeat(size) + "\n";
+// for(let i = 1; i <= size; i++){
+//     if(i % 2 == 0){
+//         space += " ";
+//     }
+//     space += sign;
+//     for(let i = size; i <= size; i++ ){
+//         space += sign.repeat(size) + "\n";
         
-    }
-}
+//     }
+// }
 
-console.log(space);
+// console.log(space);
