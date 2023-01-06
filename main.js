3b750d3c80025560c444e8cd780be51f10496558
Pawel-Markowicz <pawel.markowicz88@gmail.com>
2023-01-06 16:28:20 +0100

    recurention

--- main.js
+++ main.js
@@ -16,3 +16,50 @@
 // }
 
 // console.log(space);
+
+// POTĘGOWANIE W REKURENCJI
+
+// function power(base, exponent){
+//     if(exponent == 0){
+//         return 1;
+//     } else {
+//         return base * power(base, exponent - 1) ;
+//     }
+// }
+
+// console.log(power(2, 5));
+
+// KANAŁ O WSZYSTKIM, REKURENCJA - rekurencja
+// to wywoływanie samej funkcji przez samą siebie
+
+// let result = document.getElementById("result");
+
+// function silnia(n){
+//     if(n == 1){
+//         return 1;
+//     } else {
+//         return n * silnia(n - 1);
+//     }
+// }
+
+// result.innerHTML += "silnia = " + silnia(4)
+
+// // 4 * 3 * 2 * 1  
+
+// REKURENCJA CIĄG DALSZY
+
+// JEŻELI N TO 1, ZATRZYMAJ SIĘ
+// JEŻELI N JEST PARZYSTE, POWTÓRZ DZIAŁANIE DLA N/2
+// JEŻELI N JEST NIEPARZYSTE, POWTÓRZ DZIAŁANIE DLA 3N + 1
+
+// function collatz(n){
+//     if(n == 1){
+//         return 0;
+//     }
+
+//     return n % 2 == 0 ? collatz(n/2) + 1 :
+//      collatz(3 * n + 1 ) + 1;
+
+// }
+
+// console.log(`ilość kroków: ${collatz(21)}`);
